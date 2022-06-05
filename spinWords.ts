8fefb65656e88a3e804d8ed3430e72389c9a64a3
Reiahx01 <ahnrei@gmail.com>
2022-06-05 01:37:56 -0400

    edit

--- spinWords.ts
+++ spinWords.ts
@@ -1,3 +1,11 @@
 const spinWords = (words: string): string => {
     return words.replace(/\w{5,}/g, word => word.split('').reverse().join(''))
 }
+
+// 2nd possible solution
+
+// const spinWords = (words: string): string => words.split(' ')
+//                                                         .map(m => m.length >= 5 
+//                                                              ? m.split('').reverse().join('') 
+//                                                              : m)
+//                                                         .join(' ')
